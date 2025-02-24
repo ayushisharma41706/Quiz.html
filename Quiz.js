c190728e872e5a3c4b6673ac67413e31aa1ae90b
ayushisharma41706 <ayushisharma41706@gmail.com>
2025-02-24 19:21:47 +0530

    Create Quiz.js

---
+++ Quiz.js
@@ -0,0 +1,77 @@
+// script.js
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const quizQuestions = [
+    {
+        question: "What is the capital of France?",
+        answers: {
+            a: "Berlin",
+            b: "Madrid",
+            c: "Paris"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who is the CEO of Tesla?",
+        answers: {
+            a: "Bill Gates",
+            b: "Elon Musk",
+            c: "Jeff Bezos"
+        },
+        correctAnswer: "b"
+    }
+];
+
+function buildQuiz() {
+    const output = [];
+
+    quizQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+
+            for (letter in currentQuestion.answers) {
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+
+    quizContainer.innerHTML = output.join('');
+}
+
+function showResults() {
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    quizQuestions.forEach((currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if (userAnswer === currentQuestion.correctAnswer) {
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'green';
+        } else {
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+}
+
+buildQuiz();
+
+submitButton.addEventListener('click', showResults);
